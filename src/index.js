b66e30b41c72c60cf0f91c771f95b49cf112e1cd
johny911 <johnyabraham8056@gmail.com>
2025-05-02 13:04:31 +0530

    v4.5

--- src/index.js
+++ src/index.js
@@ -11,3 +11,11 @@ root.render(
     <App />
   </StrictMode>
 );
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then(reg => console.log("✅ Service worker registered"))
+      .catch(err => console.log("❌ SW registration failed:", err));
+  });
+}
